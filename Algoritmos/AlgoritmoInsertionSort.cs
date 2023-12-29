ae351ef6fc6fb1a3cc0ed2ebddffb0048d0fcb02
Lucas Dirani <lruizd@protonmail.com>
2023-12-29 08:58:06 -0300

    Adicionar arquivos de projeto.

---
+++ Algoritmos/AlgoritmoInsertionSort.cs
@@ -0,0 +1,21 @@
+﻿namespace InsertionSort.App.Algoritmos
+{
+    public static class AlgoritmoInsertionSort
+    {
+        public static void InsertionSort(int[] arr, int n)
+        {
+            int i, j, chave;
+            for (i = 1; i < n; i++)
+            {
+                chave = arr[i];
+                j = i - 1;
+                while (j >= 0 && arr[j] > chave)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = chave;
+            }
+        }
+    }
+}
